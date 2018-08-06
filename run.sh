717273d1c16ff98af65aedde65ee668c3df05f41
Zaki <zaki.zuberi@gmail.com>
2018-08-05 22:08:00 -0700

    Initial commit

---
+++ run.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+#
+# Use this shell script to compile (if necessary) your code and then execute it. Below is an example of what might be found in this file if your program was written in Python
+#
+python ./src/pharmacy-counting.py ./input/de_cc_data.txt ./output/top_cost_drug.txt
